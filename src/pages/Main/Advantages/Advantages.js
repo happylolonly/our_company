41dc617dbd538a098a4111ee0ca84f363201ebe4
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-04-16 19:12:23 +0300

    add advantages section

---
+++ src/pages/Main/Advantages/Advantages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Advantages.scss';
+
+const propTypes = {};
+
+const Advantages = () => {
+  return <div className="advantages">saas</div>;
+};
+
+Advantages.propTypes = propTypes;
+
+export default Advantages;
